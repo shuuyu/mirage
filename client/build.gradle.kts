63745c7d61f2a4f097daecb5d54a2606dba27fc4
yujin <84204420+euphorxa@users.noreply.github.com>
2022-11-30 18:01:50 -0800

    I need to commit this before I do something funny

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
     id("io.github.juuxel.loom-quiltflower")
 }
 
-dependencies {
+quiltflower {
+    quiltflowerVersion.set("1.9.0")
+}
 
+dependencies {
+    minecraft("com.mojang:minecraft:1.19.2")
+    mappings("net.fabricmc:yarn:1.19.2+build.28:v2")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.67.0+1.19.2")
+    modImplementation("net.fabricmc:fabric-loader:0.14.10")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.8.7+kotlin.1.7.22")
 }
